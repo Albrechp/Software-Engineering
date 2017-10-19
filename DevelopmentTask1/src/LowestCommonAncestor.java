764b75fa4af4514f492ea3828b4fcc7d8638d565
philip <albrechp@tcd.ie>
2017-10-19 23:09:04 +0100

    created project

---
+++ DevelopmentTask1/src/LowestCommonAncestor.java
@@ -0,0 +1,17 @@
+
+public class LowestCommonAncestor {
+
+	public static void main(String[] args) {
+		
+
+		System.out.println("hallo world");
+		System.out.println(t());
+
+	}
+	
+	public static boolean t()
+	{
+		return false;
+	}
+
+}
---
+++ DevelopmentTask1/src/LowestCommonAncestorTest.java
@@ -0,0 +1,18 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class LowestCommonAncestorTest extends LowestCommonAncestor{
+
+	@Test
+	public void test() {
+
+		  Assert.assertTrue(true);
+		  Assert.assertFalse(false);
+		  Assert.assertEquals(false, t());
+	}
+
+
+}
