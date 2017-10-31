673f050f752d411a133c495dc99350b6f60058c6
philip <albrechp@tcd.ie>
2017-10-31 22:41:41 +0000

    created DevelopmentTask2

---
+++ DevelopmentTask2/src/DirectedAcyclicGraph.java
@@ -0,0 +1,7 @@
+
+public class DirectedAcyclicGraph {
+
+	public static void main(String[] args) {
+		System.out.println("hallo");
+	}
+}
---
+++ DevelopmentTask2/src/DirectedAcyclicGraphTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class DirectedAcyclicGraphTest {
+
+	@Test
+	public void test() {
+		Assert.assertEquals(true, true);
+	}
+
+}
